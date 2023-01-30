c7b1750ecda12b35c51c758ff8329a9a6d7bdb57
MorganKennedyCognizant <116623200+MorganKennedyCognizant@users.noreply.github.com>
2023-01-30 14:14:26 +1100

    Align items

--- REAssignment/App.tsx
+++ REAssignment/App.tsx
@@ -9,19 +9,26 @@ export default function App() {
       <View style={{
         backgroundColor: "#fff",
         flex: 1,
+        flexDirection: "row", // horizontal axis = Main primary
+        justifyContent: "center",  // Main
+        alignItems: "center", // Seconday
       }}
       >
         <View style={{
           backgroundColor: "dodgerblue",
-          flex: 2,
+          width: 100,
+          height: 100,
+          alignSelf: "flex-start"
         }}/>
         <View style={{
           backgroundColor: "gold",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}/>
         <View style={{
           backgroundColor: "tomato",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}/>
       </View>
     </SafeAreaView>
